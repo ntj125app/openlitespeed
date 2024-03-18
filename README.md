e025f4df2e98b778e48086fb8b02b8bbdabace3a
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-03-18 11:25:46 +0000

    Consolidated 03 Oct 2024

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Build Run](https://github.com/ntj125app/openlitespeed/actions/workflows/docker-build.yaml/badge.svg)](https://github.com/ntj125app/openlitespeed/actions/workflows/docker-build.yaml)
+
+# What is this repository?
+
+This is customized Openlitespeed repository for use in other application
+This repo already apply some of the latest patches and pear / pearl packages for easier setup
+
+## LICENSE
+
+License can be read in [LICENSE](https://github.com/ntj125app/openlitespeed/blob/latest/LICENSE)
---
+++ docker-base/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# init PID
+pid=0
+
+# SIGTERM signal handlers
+sigterm_handler() {
+    if [ $pid -ne 0 ]; then
+        kill -s SIGTERM $pid
+        wait $pid
+    fi
+    exit 143; # 128 + 15 -- SIGTERM
+}
+
+# CTRL + C signal handlers
+abort_handler() {
+    if [ $pid -ne 0 ]; then
+        kill -s SIGTERM $pid
+        wait $pid
+    fi
+    exit 130; # 128 + 2 -- SIGINT
+}
+
+trap 'sigterm_handler' SIGTERM
+trap 'abort_handler' SIGINT INT
+
+# run application if no ARGS / CMD
+if [ $# -eq 0 ]; then
+    /usr/local/lsws/bin/litespeed
+else
+    exec "$@"
+fi
+
+# container healthcheck
+while true; do
+    if [ $# -eq 0 ]; then
+        if ! /usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running' > /dev/null; then
+            /usr/local/lsws/bin/lswsctrl status >&1
+            exit 143; # 128 + 15 -- SIGTERM
+        fi
+    fi
+
+    # prevent container from exiting and get stdout
+    tail -f /usr/local/lsws/logs/error.log >&1 &
+    wait ${!}
+done
